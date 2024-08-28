ce68673637d647771e567f0ea24ac0ee4f146c80
Amarjeet <amarjeetofficial81@gmail.com>
2024-08-28 21:12:19 +0530

    add resume upload feature

---
+++ app.js
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+import userData from "./routes/userdata.js";
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.static('./public'))
+app.use(express.json())
+
+app.use('/user-data', userData)
+
+const __filepath = fileURLToPath(import.meta.url)
+
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__filepath, '../public', 'index.html'))
+})
+
+app.listen(port, ()=>{
+    console.log(`App is listening on post ${port}`)
+})
