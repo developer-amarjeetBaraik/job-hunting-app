d376f7ea3bf8c4db71616ff5089d39c663b27672
Amarjeet <amarjeetofficial81@gmail.com>
2024-08-29 17:21:34 +0530

    bug fix

--- vercel-deploy/api/app.js
+++ vercel-deploy/api/app.js
@@ -1,8 +1,33 @@
-import express from 'express';
-const app = express();
+import 'dotenv/config'
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+import userData from "./vercel-deploy/api/routes/userdata.js";
 
-app.get('/hello', (req, res) => {
-  res.send('Hello, world!');
-});
+const app = express()
+const port = process.env.PORT
+
+app.use(express.static('./public'))
+app.use(express.json())
+
+app.use('/user-data', userData)
+
+const __filepath = fileURLToPath(import.meta.url)
+
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__filepath,'./public', 'index.html'))
+})
+app.get('/hello',(req, res)=>{
+    res.send('hello from server')
+})
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello, world!' });
+  });
+
+// app.listen(port, ()=>{
+//     console.log(`App is listening on post ${port}`)
+// })
 
 export default app;
