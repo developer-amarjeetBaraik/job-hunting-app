58eb1e7085d86f9c46a55af002932258d70fcf13
Amarjeet <amarjeetofficial81@gmail.com>
2024-08-29 12:22:25 +0530

    bug fix

--- vercel-deploy/api/app.js
+++ vercel-deploy/api/app.js
@@ -16,7 +16,7 @@ const __filepath = fileURLToPath(import.meta.url)
 
 
 app.get('/',(req, res)=>{
-    res.sendFile(path.join('./public', 'index.html'))
+    res.sendFile(path.join(__filepath,'./public', 'index.html'))
 })
 app.get('/hello',(req, res)=>{
     res.send('hello from server')
