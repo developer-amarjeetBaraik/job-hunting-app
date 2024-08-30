c89bbb984d3e6ee19ff25dd6de156763906e6f6c
Amarjeet <amarjeetofficial81@gmail.com>
2024-08-30 08:58:18 +0530

    update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/user-data', userData)
 app.get('/',(req, res)=>{
     // res.send('home page')
     console.log(path.join(__basepath, './public', 'index.html'))
-    res.sendFile(path.join(__filepath, './public', 'index.html'))
+    res.sendFile(path.join(__dirname, './public', 'index.html'))
 })
 
 app.get('/hello',(req, res)=>{
