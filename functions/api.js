049120b3908167fd2df6e0bdc6ffbcec10ed6f6f
Amarjeet <amarjeetofficial81@gmail.com>
2024-08-31 09:59:26 +0530

    fix api.js file

--- functions/api.js
+++ functions/api.js
@@ -16,26 +16,26 @@ const __basepath = path.dirname(__filepath)
 app.use(express.static(path.join('public')))
 app.use(express.json())
 
-app.use('/.netlify/functions/api', router);
 app.use('/user-data', userData)
 
 
-app.get('/',(req, res)=>{
+router.get('/',(req, res)=>{
     // res.send('home page')
     console.log(path.join(__dirname, './public', 'index.html'))
     res.sendFile(path.join(__dirname, './public'))
 })
 
-app.get('/hello',(req, res)=>{
+router.get('/hello',(req, res)=>{
     res.send('hello from server')
 })
 
-app.get('/api/hello', (req, res) => {
+router.get('/api/hello', (req, res) => {
     res.json({ message: 'Hello, world!' });
-  });
+});
 
-app.listen(port, ()=>{
+router.listen(port, ()=>{
     console.log(`App is listening on post ${port}`)
 })
 
+app.use('/.netlify/functions/api', router);
 export const handler = serverless(app);
